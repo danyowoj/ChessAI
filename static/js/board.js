c6d4f7f8328fca3213508363be3ec8445d985536
danyowoj <latsuk1803@gmail.com>
2025-06-16 10:50:19 +0700

    Chess v.1 - Complete a game for two players

--- static/js/board.js
+++ static/js/board.js
@@ -1,119 +1,112 @@
-$(document).ready(function () {
-    var board = null;
-    var game = new Chess();
-    var $board = $('#board');
-    var $status = $('#status');
-    var $modal = $('#gameOverModal');
-    var $gameResultText = $('#gameResultText');
+let board = null;
+let game = new Chess();
 
-    // Функция для обновления подсветки текущего игрока
-    function updatePlayerHighlight() {
-        $board.parent()
-            .removeClass('white-turn black-turn')
-            .addClass(game.turn() === 'w' ? 'white-turn' : 'black-turn');
+function updateStatus() {
+    const statusEl = document.getElementById('status');
+    const moveColor = game.turn() === 'w' ? 'Ход белых' : 'Ход чёрных';
+    statusEl.textContent = moveColor;
+    
+    // Обновляем подсветку текущего игрока
+    const boardElement = document.getElementById('board');
+    boardElement.classList.remove('white-turn', 'black-turn');
+    boardElement.classList.add(game.turn() === 'w' ? 'white-turn' : 'black-turn');
+}
+
+function removeGreySquares() {
+    $('#board .square-55d63').css('background', '');
+}
+
+function greySquare(square) {
+    const $square = $('#board .square-' + square);
+    const background = $square.hasClass('black-3c85d') ? '#696969' : '#a9a9a9';
+    $square.css('background', background);
+}
+
+function onMouseoverSquare(square, piece) {
+    // Подсвечивать только фигуры текущего игрока
+    if (!piece || piece[0] !== game.turn()) return;
+
+    const moves = game.moves({ square, verbose: true });
+    if (moves.length === 0) return;
+
+    greySquare(square);
+    moves.forEach(move => greySquare(move.to));
+}
+
+function onMouseoutSquare() {
+    removeGreySquares();
+}
+
+function onDragStart(source, piece) {
+    // Запретить перетаскивание, если:
+    // - Игра закончена
+    // - Не очередь текущего игрока
+    if (game.game_over() || piece[0] !== game.turn()) {
+        return false;
     }
+}
 
-    // Функция для подсветки возможных ходов
-    function highlightPossibleMoves(square) {
-        // Очищаем предыдущие подсветки
-        $board.find('.square-55d63').removeClass('highlight possible-move');
+function onDrop(source, target) {
+    removeGreySquares();
+    $('.suggested-move').removeClass('suggested-move');
 
-        // Подсвечиваем текущую клетку
-        $board.find('.square-' + square).addClass('highlight');
+    const move = game.move({ from: source, to: target, promotion: 'q' });
+    if (!move) return 'snapback';
 
-        // Получаем возможные ходы
-        var moves = game.moves({
-            square: square,
-            verbose: true
-        });
-
-        // Подсвечиваем возможные ходы
-        moves.forEach(function (move) {
-            $board.find('.square-' + move.to).addClass('possible-move');
-        });
-    }
-
-    // Функция показа модального окна
-    function showGameOverModal(message) {
-        $gameResultText.text(message);
-        $modal.css('display', 'flex');
-    }
-
-    // Функция скрытия модального окна
-    function hideGameOverModal() {
-        $modal.hide();
-    }
-
-    function updateStatus() {
-        var moveColor = game.turn() === 'w' ? 'белых' : 'черных';
-
-        if (game.in_checkmate()) {
-            var winner = game.turn() === 'w' ? 'черных' : 'белых';
-            $status.text('Мат. Победа ' + winner);
-            showGameOverModal('Победа ' + winner + '!');
-        } else if (game.in_draw()) {
-            $status.text('Ничья');
-            showGameOverModal('Ничья!');
-        } else {
-            $status.text('Ход ' + moveColor);
-        }
-
-        // Обновляем подсветку текущего игрока
-        updatePlayerHighlight();
-    }
-
-    function onDragStart(source, piece) {
-        if (game.game_over()) return false;
-
-        // Проверка, чья фигура
-        if ((game.turn() === 'w' && piece.startsWith('b')) ||
-            (game.turn() === 'b' && piece.startsWith('w'))) {
-            return false;
-        }
-
-        // Подсвечиваем возможные ходы
-        highlightPossibleMoves(source);
-    }
-
-    function onDrop(source, target) {
-        // Очищаем подсветку
-        $board.find('.square-55d63').removeClass('highlight possible-move');
-
-        var move = game.move({
-            from: source,
-            to: target,
-            promotion: 'q'
-        });
-
-        if (move === null) return 'snapback';
-
-        board.position(game.fen());
-        updateStatus();
-    }
-
-    function onSnapEnd() {
-        board.position(game.fen());
-    }
-
-    // Обработчик для кнопки рестарта
-    $('#restartButton').click(function() {
-        game.reset();
-        board.start();
-        $board.find('.square-55d63').removeClass('highlight possible-move');
-        hideGameOverModal();
-        updateStatus();
-    });
-
-    board = Chessboard('board', {
-        draggable: true,
-        position: 'start',
-        pieceTheme: '/static/chessboard/img/chesspieces/wikipedia/{piece}.png',
-        onDragStart: onDragStart,
-        onDrop: onDrop,
-        onSnapEnd: onSnapEnd
-    });
-
-    // Инициализация подсветки
-    updatePlayerHighlight();
+    board.position(game.fen());
     updateStatus();
+
+    if (game.game_over()) {
+        showGameResult();
+    }
+}
+
+function showGameResult() {
+    let resultText = '';
+    if (game.in_checkmate()) {
+        resultText = game.turn() === 'w' ? 'Чёрные поставили мат! Победа чёрных.' : 'Белые поставили мат! Победа белых!';
+    } else if (game.in_draw()) {
+        resultText = 'Игра закончилась вничью.';
+    } else if (game.in_stalemate()) {
+        resultText = 'Пат. Ничья.';
+    } else if (game.in_threefold_repetition()) {
+        resultText = 'Ничья по троекратному повторению.';
+    } else if (game.insufficient_material()) {
+        resultText = 'Ничья из-за недостатка материала.';
+    }
+
+    if (resultText) {
+        $('#resultText').text(resultText);
+        $('#resultModal').show();
+    }
+}
+
+const config = {
+    draggable: true,
+    position: 'start',
+    pieceTheme: '/static/chessboard/img/chesspieces/wikipedia/{piece}.png',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare
+};
+
+$(document).ready(function () {
+    board = Chessboard('board', config);
+    board.start();
+    updateStatus();
+
+    // Кнопка "сдаться"
+    $('#resignBtn').on('click', () => $('#confirmResign').show());
+    $('#confirmYes').on('click', () => {
+        $('#confirmResign').hide();
+        const loser = game.turn() === 'w' ? 'Белые' : 'Чёрные';
+        $('#resultText').text(`${loser} сдались.`);
+        $('#resultModal').show();
+    });
+    $('#confirmNo').on('click', () => $('#confirmResign').hide());
+});
+
+window.addEventListener('resize', () => {
+    if (board) board.resize();
 });
